5b39b1d0b338d9e2f0d63b44b4a1c645d8a63073
shitaaaa <124363356+shitaaaa@users.noreply.github.com>
2026-04-30 17:25:45 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,6 +1,6 @@
 
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://shitaaaa.github.io');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
